6e53304efc800df0cdb743829e790ca1eff8bbab
george <hasdro@mail.bg>
2024-03-14 19:03:57 +0000

    Requesting input from the user

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,7 @@
 # prints a simple message to the terminal 
-print("Git is Awesome!")
+def my_function():
+    user_name = input("Please enter your name: ")
+    print("Hello, " + user_name + "! You have entered the Git Universe!")
+    
+my_function()
+    
