68b43e3751b2838872e62e967ff57402fae0be34
george-ian <162318390+george-ian@users.noreply.github.com>
2024-03-14 21:15:21 +0000

    Delete

--- helloWorld.py
+++
@@ -1,7 +0,0 @@
-# prints a simple message to the terminal 
-def my_function():
-    user_name = input("Please enter your name: ")
-    print("Hello, " + user_name + "! You have entered the Git Universe!")
-    
-my_function()
-    
